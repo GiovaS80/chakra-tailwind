c24c47561e33a1a643d8f2cd68a0b8fecd59c963
Gio <giomain.pv@gmail.com>
2023-07-11 17:26:02 +0700

    after so many attempts, it seems that Tailwind and Chakra-Ui want to clooaborate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+// import Link from 'next/link'
+// import Layout from '../components/Layout'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+// const IndexPage = () => (
+//   <Layout title="Home | Next.js + TypeScript Example">
+//     <h1 className="text-6xl font-bold underline">Hello Next.js 👋</h1>
+//     <p>
+//       <Link href="/about">About</Link>
+//     </p>
+//   </Layout>
+// )
 
-export default IndexPage
+// export default IndexPage
+
+import Head from "next/head";
+import { Heading,} from "@chakra-ui/react"
+
+export default function Home() {
+  return (
+    <div className={"styles.container"} >
+      <Head>
+        <title>kkk</title>
+
+      </Head>
+
+      <main>
+        <h1 className="text-7xl font-bold underline">GIOOOOOOOOOOO</h1>
+        <Heading className="font-bold underline" >Ciaooooooooo</Heading>
+      </main>
+    </div>
+  )
+}
